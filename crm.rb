d46addf436b034fe7aa075ba808dbb870c6a9a24
Tom Billingsley <tom.billingsley@gmail.com>
2015-04-06 15:14:13 -0400

    comitting a working (complete) first draft of crm.rb

--- crm.rb
+++ crm.rb
@@ -35,9 +35,8 @@ class CRM
             }
            ]
 
-    
     data.each do |d|
-      @rolodex.add(d =>first_name, d =>last_name, d=>email, d=>note)
+      @rolodex.add d[:first_name], d[:last_name], d[:email], d[:note] 
     end
   end
 
@@ -52,13 +51,16 @@ class CRM
   end
 
   def main_menu
-    puts "main_menu"
-    print_main_menu
-    user_selected = gets.to_i
-    call_option(user_selected)
+    user_selected = 0
+    while user_selected != 6  
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
+    puts "[0] Display a contact" 
     puts "[1] Add a new contact"
     puts "[2] Modify an existing contact"
     puts "[3] Delete a contact"
@@ -68,7 +70,16 @@ class CRM
     puts "Enter a number: "
   end
 
+  def print_attribute_menu 
+    puts "[1] First name"
+    puts "[2] Last name" 
+    puts "[3] Email"
+    puts "[4] Note"
+    puts "Enter a number, or 0 (zero) to cancel. "
+  end
+
   def call_option(user_selected)
+    display_a_contact if user_selected == 0 
     add_new_contact if user_selected == 1
     modify_existing_contact if user_selected == 2
     delete_a_contact if user_selected == 3 
@@ -77,12 +88,23 @@ class CRM
     exit if user_selected == 6 
   end
 
+  def display_all_contacts
+    puts @rolodex.display_all_contacts
+  end
+
+  def display_a_contact
+    puts "Please enter a contact ID."
+    id = gets.chomp.to_i
+    puts @rolodex.display_particular_contact(id)
+  end
+
   def self.run(name)
     crm = CRM.new(name)
     crm.main_menu
   end
 
   def get_contact_info_from_user
+    userdata = []
     puts "Please enter first name for new contact." 
     first = gets.chomp()
     puts "Please enter last name" 
@@ -91,18 +113,15 @@ class CRM
     email = gets.chomp()
     puts "Please enter notes for your new contact. "
     notes = gets.chomp()
-  end 
+    userdata.push(first)
+    userdata.push(last)
+    userdata.push(email)
+    userdata.push(notes)
+  end
 
   def add_new_contact
-    print "Enter First Name: "
-    first_name = gets.chomp
-    print "Enter Last Name: "
-    last_name = gets.chomp
-    print "Enter Email Address: "
-    email = gets.chomp
-    print "Enter a Note: "
-    note = gets.chomp
-    contact = Contact.new(first_name, last_name, email, note)
+    userdata = get_contact_info_from_user
+    contact = @rolodex.add(userdata[0], userdata[1], userdata[2], userdata[3])
   end
 
   def get_user_id 
@@ -112,19 +131,47 @@ class CRM
     confirm = gets.chomp
     if confirm[0] == 'Y' or confirm[0] == 'y' then
       return id
-      return nil
     end 
   end
 
   def modify_existing_contact
     id = get_user_id 
     if id then 
-      contact = @contacts[id]
-      get_contact_info_from_user
-      @rolodex.modify_contact(id, first_name, last_name, email, note)
+      contact = @rolodex.find(id)
+
+      @rolodex.display_particular_contact(id)
+      puts "\n\n" 
+      userdata = print_attribute_menu
+      attribute_no = gets.chomp.to_i
+
+      msg = ""
+      case attribute_no
+      when 1
+        msg = "first_name"
+      when 2
+        msg = "last_name"
+      when 3
+        msg = "email"
+      when 4
+        msg = "note"
+      end
+
+      puts "Please enter an updated value for #{msg}"
+      val = gets.chomp
+
+      @rolodex.set_attrib_value(id, msg, val)
+      @rolodex.display_particular_contact(id)
     end
   end 
 
+  def display_an_attribute
+    puts "Please enter an attribute"
+    key = gets.chomp
+    puts "Please enter a value"
+    val = gets.chomp
+    keysym = key.to_sym
+    puts @rolodex.display_info_by_attribute(key, val)
+  end
 end
 
 CRM.run("boo")
