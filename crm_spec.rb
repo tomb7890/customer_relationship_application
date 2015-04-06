2eacb49e6f7f6ffe1ddcd0bde13fa9212df06970
Tom Billingsley <tom.billingsley@gmail.com>
2015-04-06 10:35:41 -0400

    get the puts calls out of the tests

--- crm_spec.rb
+++ crm_spec.rb
@@ -1,59 +1,107 @@
 require_relative "contact"
 require_relative "rolodex"
 
-describe "Salestax Addl"  do  
+describe "CRM tests"  do  
 
-testdata = [
-            {
-              first_name: "Frederick",
-              last_name: "Flintstone",
-              email: "fredflintstone@paleo.org",
-              note: "Willlllllllllllllllllllllllllmaaaaaaaaaaaaaaaaaaaaaaaaa!!!!!!!."
-            },
+  before(:all) do
 
-            {
-              first_name: "Barney",
-              last_name: "Rubble",
-              email: "b.rubble@paleo.org",
-              note: "stuff"
-            },
+    @testdata = [
+                {
+                  first_name: "Frederick",
+                  last_name: "Flintstone",
+                  email: "fredflintstone@paleo.org",
+                  note: "Wilmaaaaaaaaaaaa!!!!!!!"
+                },
+                
+                {
+                  first_name: "Barney",
+                  last_name: "Rubble",
+                  email: "b.rubble@paleo.org",
+                  note: "stuff"
+                },
+                
+                {
+                  first_name: "F. Ross",
+                  last_name: "Johnson",
+                  email: "rossjohnson@hotmail.com",
+                  note: "Such a cool dude."
+                },
+                
+                {
+                  first_name: "David S.",
+                  last_name: "Johnson",
+                  email: "david.s.johnson@gmail.com",
+                  note: "Foo bar baz."
+                },
+                
+                {
+                  first_name: "Frederick",
+                  last_name: "Carton de Wiart",
+                  email: "adrianwiart@gmail.com",
+                  note: "Even cooler."
+                }
+               ]
+    
+    @r= Rolodex.new
+    @testdata.each do |d|
+      @r.add d[:first_name], d[:last_name], d[:email], d[:note] 
+    end
 
-            {
-              first_name: "F. Ross",
-              last_name: "Johnson",
-              email: "rossjohnson@hotmail.com",
-              note: "Such a cool dude."
-            },
-            {
-              first_name: "Adrian",
-              last_name: "Carton de Wiart",
-              email: "adrianwiart@gmail.com",
-              note: "Even cooler."
-            }
-           ]
-
+  end
 
   it "should verify basic testing" do
     expect("5 5").to eql "5 5"
   end
   
   it "should initialize the application" do 
-    r= Rolodex.new
-    testdata.each do |d|
-      r.add d[:first_name], d[:last_name], d[:email], d[:note] 
-    end
+    @r
   end
 
   it "should display_all_contacts" do 
-    r= Rolodex.new
-    testdata.each do |d|
-      r.add d[:first_name], d[:last_name], d[:email], d[:note] 
-    end
-
-    puts r.display_all_contacts
+    stuff= @r.display_all_contacts
+    expected = @testdata.length 
+    actual = stuff.count("\n")
+    expect(actual).to eql expected
   end
 
-  
+  it "should display a particular contact" do
+    id = 1003 
+    expected = "F. Ross Johnson rossjohnson@hotmail.com Such a cool dude. 1003"
+    d = @r.display_particular_contact(id)
+    expect(expected).to eql d 
+  end
+
+  it "should delete a particular contact" do 
+    # first verify we can find a record 
+    id = 1003 
+    expected = "F. Ross Johnson rossjohnson@hotmail.com Such a cool dude. 1003"
+    d = @r.display_particular_contact(id)
+    expect(expected).to eql d 
+
+    expect(@r.size).to eql 5
+    @r.delete_contact(id) 
+    expect(@r.size).to eql 4
+    
+    expected = nil
+    d = @r.display_particular_contact(id)
+    expect(expected).to eql d
+  end	
+
+  it "should find info by attrib " do
+    expected = "b.rubble@paleo.org"
+    info = expected
+    expect(info).to eql expected 
+  end  
+
+  it "should display an attribute" do
+    key =  "first_name"
+    val = "Frederick"
+    s = @r.display_info_by_attribute(key, val)
+
+    # There should be two records with the first name Fred 
+    n = s.count("\n")
+    expect(n).to eql 2 
+  end
 end
 
 
