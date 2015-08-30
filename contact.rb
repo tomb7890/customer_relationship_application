705a770a271707b13115ebb2bc3617bee203fe10
Tom Billingsley <tomb@antec.local>
2015-08-30 16:18:54 -0400

    modified for use with database/activerecord

--- contact.rb
+++ contact.rb
@@ -1,16 +1,12 @@
-class Contact
-
-  attr_accessor :id, :first_name, :last_name, :email, :note
-
-  def initialize(id, first_name, last_name, email, note)
-    @first_name = first_name
-    @last_name = last_name
-    @email = email
-    @note = note
-    @id = id 
-  end
-
+class Contact < ActiveRecord::Base
   def to_s
-    "#{@first_name} #{@last_name} #{@email} #{@note} #{id}"
+    str = <<-HERE
+    First name: #{first_name}
+    Last name: #{last_name}
+    Email: #{email}
+    Notes: #{notes}
+    I.D.: #{id}
+    HERE
+    str
   end
 end
