ce6e81974f2e991654c695558cf304c20095db34
Tom Billingsley <tomb@antec.local>
2015-08-31 12:54:08 -0400

    first stab at test suite framework

---
+++ spec/contact_spec.rb
@@ -0,0 +1,21 @@
+require_relative "../connection"
+require_relative "../rolodex"
+require_relative "../crm.rb"
+
+describe Contact do
+
+  describe '#new' do
+    it "returns a Contact object" do
+      c = Contact.new
+      expect(c).to be_a Contact
+    end
+  end
+
+  describe '#to_s' do
+    it "returns the contact's details as a string" do
+      c = Contact.last
+      expected = c.to_s
+      expect(expected).to eq "Frederick Carton de Wiart adrianwiart@gmail.com Even cooler. 97"
+    end
+  end
+end
---
+++ spec/crm_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../connection"
+require_relative "../rolodex"
+require_relative "../crm.rb"
+
+describe CRM do
+  describe '#serve_menu_response' do
+    it "should blah blah blah" do
+
+    end
+  end
+  describe '#sanitize_name' do
+  end
+  describe '#sanitize_notes' do
+  end
+  describe '#sanitize_email' do
+  end
+end
