a621d45292a19171617c57f405e48f1df9a16da1
Tom Billingsley <tomb@antec.local>
2015-09-01 09:19:11 -0400

    configuration environment as parameter

--- connection.rb
+++ connection.rb
@@ -3,7 +3,21 @@ require "yaml"
 require "sqlite3"
 require "logger"
 
-data_base_configuration = YAML::load(File.open('database.yml'))
+conf = 'default'
+rails_env = ENV['RAILS_ENV']
+
+ARGV.each do |a|
+  elements = a.split("=")
+  if elements[0] == 'ENV'
+    conf = elements[1]
+  end
+end
+
+if not rails_env.empty?
+  conf = rails_env
+end
+
+yamlfile = YAML::load(File.open('database.yml'))
+data_base_configuration = yamlfile[conf]
 ActiveRecord::Base.establish_connection(data_base_configuration)
 ActiveRecord::Base.logger = Logger.new(File.open("database.log", "w"))
-
--- lib/fixturedata.rb
+++ lib/fixturedata.rb
@@ -1,1 +1,18 @@
 #!/usr/bin/env ruby
+
+require "yaml"
+require "sqlite3"
+require_relative "../connection"
+require_relative "../rolodex"
+
+elements = YAML::load(File.open('test/fixtures/contacts.yml'))
+elements.each do |el|
+  hsh = el[1]
+
+  f = hsh["first_name"]
+  l = hsh["last_name"]
+  em = hsh["email"]
+  n = hsh["notes"]
+
+  Contact.create!(first_name: f, last_name: l, email: em, notes: n)
+end
