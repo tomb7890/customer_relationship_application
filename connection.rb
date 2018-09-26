2cc7ca98889f5170a9de0d45825c15a6b8c27682
Tom Billingsley <tom.billingsley@gmail.com>
2018-09-26 14:49:51 -0400

    Remove unnecessary ARGV processing

--- connection.rb
+++ connection.rb
@@ -1,23 +1,11 @@
-require "active_record"
-require "yaml"
-require "sqlite3"
-require "logger"
+require 'active_record'
+require 'yaml'
+require 'sqlite3'
+require 'logger'
 
-conf = 'default'
-rails_env = ENV['RAILS_ENV']
-
-ARGV.each do |a|
-  elements = a.split("=")
-  if elements[0] == 'ENV'
-    conf = elements[1]
-  end
-end
-
-if not rails_env.empty?
-  conf = rails_env
-end
+conf = ENV['RAILS_ENV'] || 'default'
 
 yamlfile = YAML::load(File.open('database.yml'))
 data_base_configuration = yamlfile[conf]
 ActiveRecord::Base.establish_connection(data_base_configuration)
-ActiveRecord::Base.logger = Logger.new(File.open("database.log", "w"))
+ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'w'))
