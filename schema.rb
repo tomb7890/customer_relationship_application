8ab35a6988a07585fcda84832b255c42681f4659
Tom Billingsley <tom.billingsley@gmail.com>
2018-09-26 14:59:04 -0400

    Wrap code in a function callable by a Rake task

--- schema.rb
+++ schema.rb
@@ -1,17 +1,14 @@
 require_relative "connection"
 
-# define a database schema
-ActiveRecord::Schema.define do
-
-  create_table :contacts, force: :cascade do |t|
-    t.string :first_name
-    t.string :last_name
-    t.string :email
-    t.text :notes
-    t.datetime :created_at, null: false
-    t.datetime :updated_at, null: false
+def load_schema_file
+  ActiveRecord::Schema.define do
+    create_table :contacts, force: :cascade do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.text :notes
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+    end
   end
-
 end
-
-
