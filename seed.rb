ae2d7e848b85b1a36207a1e3993522e8226ffc09
Tom Billingsley <tom.billingsley@gmail.com>
2018-09-28 16:37:24 -0400

    Replace literal integer with meaningful method name

--- seed.rb
+++ seed.rb
@@ -2,12 +2,16 @@ require_relative 'connection'
 require_relative 'contact'
 require 'faker'
 
+def seed_data_amount
+  7
+end
+
 def clean_database
   Contact.delete_all
 end
 
 def seed_database
-  5.times do
+  seed_data_amount.times do
     Contact.create!(
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
