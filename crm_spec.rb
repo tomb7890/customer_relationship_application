254d52dc5ee1057d4eb199464bec8283d0455532
Tom Billingsley <tom.billingsley@gmail.com>
2018-09-28 13:45:26 -0400

    Eschew inline hardcoded test data

--- crm_spec.rb
+++ crm_spec.rb
@@ -1,53 +1,15 @@
-require_relative "contact"
-require_relative "rolodex"
+require_relative 'connection'
+require_relative 'rolodex'
+require_relative 'seed'
+require_relative 'schema'
 
-describe "CRM tests"  do  
-
-  before(:all) do
-
-    @testdata = [
-                {
-                  first_name: "Frederick",
-                  last_name: "Flintstone",
-                  email: "fredflintstone@paleo.org",
-                  note: "Wilmaaaaaaaaaaaa!!!!!!!"
-                },
-                
-                {
-                  first_name: "Barney",
-                  last_name: "Rubble",
-                  email: "b.rubble@paleo.org",
-                  note: "stuff"
-                },
-                
-                {
-                  first_name: "F. Ross",
-                  last_name: "Johnson",
-                  email: "rossjohnson@hotmail.com",
-                  note: "Such a cool dude."
-                },
-                
-                {
-                  first_name: "David S.",
-                  last_name: "Johnson",
-                  email: "david.s.johnson@gmail.com",
-                  note: "Foo bar baz."
-                },
-                
-                {
-                  first_name: "Frederick",
-                  last_name: "Carton de Wiart",
-                  email: "adrianwiart@gmail.com",
-                  note: "Even cooler."
-                }
-               ]
-    
-    @r= Rolodex.new
-    @testdata.each do |d|
-      @r.add d[:first_name], d[:last_name], d[:email], d[:note] 
-    end
-
+RSpec.configure do |config|
+  config.before(:suite) do
+    clean_database
+    load_schema_file
+    seed_database
   end
+end
 
   it "should verify basic testing" do
     expect("5 5").to eql "5 5"
